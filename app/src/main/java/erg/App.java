e72cb511edf9cf0a6fa506bea139e3f02fb2e04d
cultab <rroarck@gmail.com>
2021-08-22 19:54:44 +0300

    added fxml of app

--- app/src/main/java/erg/App.java
+++ app/src/main/java/erg/App.java
@@ -3,10 +3,13 @@
  */
 package erg;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -15,14 +18,22 @@ public class App extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + " and shiet.");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion);
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+            Scene scene = new Scene(root, 640, 480);
+            stage.setTitle("testing title");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
