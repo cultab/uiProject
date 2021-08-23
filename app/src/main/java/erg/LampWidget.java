a4fcc57fef44e66a6602ebc919de24ec7bf3924d
Asimakis Argyropoulos <makis31179@hotmail.com>
2021-08-23 15:21:19 +0300

    Lamp Widget untested

---
+++ app/src/main/java/erg/LampWidget.java
@@ -0,0 +1,29 @@
+
+package erg;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class LampWidget extends AnchorPane {
+
+    public LampWidget() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/erg/LampWidget.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+            throw new RuntimeException(exception);
+        }
+    }
+}
