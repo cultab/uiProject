46441bcbf1914e830033d1880fdfb599dfb9b73b
cultab <rroarck@gmail.com>
2021-08-24 13:39:38 +0300

    use pane for details

--- app/src/main/java/erg/AppController.java
+++ app/src/main/java/erg/AppController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 
 public class AppController implements Initializable {
 
@@ -26,14 +27,16 @@ public class AppController implements Initializable {
     private SplitPane mainSplit;
     @FXML
     private FlowPane flow;
+    @FXML
+    private Pane details;
 
     public AppController() {
         devices = new HashMap<String, Device>();
         rooms = new HashMap<String, Room>();
-        gen_rooms_and_devices();
-
-        save_devices();
-        save_rooms();
+//         gen_rooms_and_devices();
+// 
+//         save_devices();
+//         save_rooms();
 
         load_devices();
         // for (var a : devices.entrySet()) {
@@ -45,45 +48,7 @@ public class AppController implements Initializable {
         // }
     }
 
-    private void gen_rooms_and_devices() {
-        var l1 = new Lamp("192.168.1.2", "room2");
-        devices.put(l1.getIP(), l1);
-        var t1 = new TemperatureSensor("192.168.1.22", "room2");
-        devices.put(t1.getIP(), t1);
-        var room2_list = new ArrayList<String>();
-        room2_list.add(l1.getIP());
-        room2_list.add(t1.getIP());
-        var room2 = new Room("room2", room2_list);
-
-        var l2 = new Lamp("192.168.1.3", "room3");
-        devices.put(l2.getIP(), l2);
-        var t2 = new TemperatureSensor("192.168.1.23", "room3");
-        devices.put(t2.getIP(), t2);
-        var room3_list = new ArrayList<String>();
-        room3_list.add(l2.getIP());
-        room3_list.add(t2.getIP());
-        var room3 = new Room("room3", room3_list);
-
-        var l3 = new Lamp("192.168.1.4", "room4");
-        devices.put(l3.getIP(), l3);
-        var room4_list = new ArrayList<String>();
-        room4_list.add(l3.getIP());
-        var room4 = new Room("room4", room4_list);
-
-        var t4 = new TemperatureSensor("192.168.1.25", "room5");
-        devices.put(t4.getIP(), t4);
-        var room5_list = new ArrayList<String>();
-        room5_list.add(t4.getIP());
-        var room5 = new Room("room5", room5_list);
-        
-
-        rooms.put(room2.getRoom_id(), room2);
-        rooms.put(room3.getRoom_id(), room2);
-        rooms.put(room4.getRoom_id(), room2);
-        rooms.put(room5.getRoom_id(), room2);
-
-
-    }
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -106,7 +71,7 @@ public class AppController implements Initializable {
 
     public void setCurrent_details(Device device) {
         if (current_details != null) {
-            mainSplit.getItems().remove(current_details);
+            details.getChildren().remove(current_details);
         }
 
         switch(device.getClass().getSimpleName()) {
@@ -118,7 +83,7 @@ public class AppController implements Initializable {
                 break;
         }
 
-        mainSplit.getItems().add(current_details);
+        details.getChildren().add(current_details);
     }
 
     public void load_devices() {
@@ -186,6 +151,45 @@ public class AppController implements Initializable {
             System.out.println(e);
             throw new RuntimeException("Could not save rooms to " + filename);
         }
+    }
+
+    protected void gen_rooms_and_devices() {
+        var l1 = new Lamp("192.168.1.2", "room2");
+        devices.put(l1.getIP(), l1);
+        var t1 = new TemperatureSensor("192.168.1.22", "room2");
+        devices.put(t1.getIP(), t1);
+        var room2_list = new ArrayList<String>();
+        room2_list.add(l1.getIP());
+        room2_list.add(t1.getIP());
+        var room2 = new Room("room2", room2_list);
+
+        var l2 = new Lamp("192.168.1.3", "room3");
+        devices.put(l2.getIP(), l2);
+        var t2 = new TemperatureSensor("192.168.1.23", "room3");
+        devices.put(t2.getIP(), t2);
+        var room3_list = new ArrayList<String>();
+        room3_list.add(l2.getIP());
+        room3_list.add(t2.getIP());
+        var room3 = new Room("room3", room3_list);
+
+        var l3 = new Lamp("192.168.1.4", "room4");
+        devices.put(l3.getIP(), l3);
+        var room4_list = new ArrayList<String>();
+        room4_list.add(l3.getIP());
+        var room4 = new Room("room4", room4_list);
+
+        var t4 = new TemperatureSensor("192.168.1.25", "room5");
+        devices.put(t4.getIP(), t4);
+        var room5_list = new ArrayList<String>();
+        room5_list.add(t4.getIP());
+        var room5 = new Room("room5", room5_list);
+        
+
+        rooms.put(room2.getRoom_id(), room2);
+        rooms.put(room3.getRoom_id(), room2);
+        rooms.put(room4.getRoom_id(), room2);
+        rooms.put(room5.getRoom_id(), room2);
+
 
     }
 }
---
+++ app/src/main/java/erg/DetailsWidget.java
@@ -0,0 +1,8 @@
+
+package erg;
+
+import javafx.scene.layout.VBox;
+
+public class DetailsWidget extends VBox {
+    public void update() {}
+}
