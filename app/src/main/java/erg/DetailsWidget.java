d72f924c67b6e1a2c2e7cb8cbdb68395a1e0499d
cultab <rroarck@gmail.com>
2021-08-29 23:42:59 +0300

    set device name before changing device room

--- app/src/main/java/erg/DetailsWidget.java
+++ app/src/main/java/erg/DetailsWidget.java
@@ -90,11 +90,11 @@ public class DetailsWidget extends CustomWidget implements Initializable {
 
         var new_room = room_name.getSelectionModel().getSelectedItem();
 
+        sensor.setRoom_name(new_room.getName());
+
         if (!sensor.getRoom_name().equals(new_room.getName())) {
             parent.changeDeviceRoom(sensor, sensor.getRoom_name(), new_room);
         }
-
-        sensor.setRoom_name(new_room.getName());
 
         for (var room : parent.getRooms()) {
             if (room.getName().equals(sensor.getRoom_name())) {
